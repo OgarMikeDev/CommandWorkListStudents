666083f2c6c56778dcb4da5edaec5a009d0cbcbe
Mike <ogar.m.v@mail.ru>
2024-04-19 11:23:10 +0300

    compare_to Ogar

--- src/main/Student.java
+++ src/main/Student.java
@@ -2,7 +2,7 @@ package main;
 
 import java.util.*;
 
-public class Student {
+public class Student implements Comparable<Student> {
     private String name; //Mike
     private int age; //23
     private String email; //mura.m.v@email.ru
@@ -63,20 +63,17 @@ public class Student {
         return Objects.hash(name, age, email, phoneNumber);
     }
 
-    public static void printInformationAboutClients() {
+    @Override
+    public int compareTo(Student o) {
+        return getName().compareTo(o.getName());
+    }
+
+    public static void printInformationAboutStudents() {
         //Rodion
     }
 
-    public static void removeClient(long phoneNumber) {
-        Iterator<Student> clientIterator = setStudents.iterator();
-        while (clientIterator.hasNext()) {
-            Student student = clientIterator.next();
-            String name = student.getName();
-            if (student.getPhoneNumber() == phoneNumber) {
-                clientIterator.remove();
-                System.out.println("Client \"" + name + "\" removed!");
-            }
-        }
+    public static void removeStudent(long phoneNumber) {
+        //Zahar Savchenko
     }
 
     public static int getCountStudents() {
